0c4c3ea9e5dd3e28135b1735cb25426ae9bf44d0
hyunchocopie <h0625jj@gmail.com>
2020-08-04 21:14:52 +0900

    Update MyPage

--- assets/images.js
+++ assets/images.js
@@ -2,6 +2,9 @@ const images = {
     logo: require('../assets/images/banana_logo.png'),
     monkey: require('../assets/images/monkey_asset.png'),
     setting: require('../assets/images/setting.png'),
+    like: require('../assets/images/heart.png'),
+    history: require('../assets/images/list.png'),
+    alarm: require('../assets/images/bell.png'),
 }
 
 export default images
--- library/utils/moveToBottom.js
+++ library/utils/moveToBottom.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-end',
-    marginBottom: 50
+    marginBottom: 80
   }
 })
 
