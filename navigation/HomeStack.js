d48ec9d41df82f10bb644a6c2eb6ccdc12261d30
syoung125 <laura120555@gmail.com>
2020-08-04 20:23:20 +0900

    Game Result 업데이트

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -29,7 +29,7 @@ const screens = {
 };
 
 const HomeStack = createStackNavigator(screens, {
-  initialRouteName: "GamePage",
+  initialRouteName: "GamePage"
 });
 
 export default createAppContainer(HomeStack);
