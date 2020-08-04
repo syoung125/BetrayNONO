6593e488833e43774e83b6de07a37b5eda8edbec
hyunchocopie <h0625jj@gmail.com>
2020-08-04 20:05:34 +0900

    Update Mypage

--- assets/images.js
+++ assets/images.js
@@ -1,6 +1,7 @@
 const images = {
     logo: require('../assets/images/banana_logo.png'),
     monkey: require('../assets/images/monkey_asset.png'),
+    setting: require('../assets/images/setting.png'),
 }
 
 export default images
